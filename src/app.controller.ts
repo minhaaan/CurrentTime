8b1a868d6725bb72d8eb3f8f86868f9b988dc79f
최민한 <chmh0411@gmail.com>
2022-05-23 01:20:44 +0900

    add: currentTime

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('currentTime')
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  @Get()
+  getCurrentTime(): any {
+    return { date: new Date() };
+  }
+}
 
+@Controller('getHello')
+export class HelloAppController {
+  constructor(private readonly appService: AppService) {}
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return 'Hello World';
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HelloAppController } from './app.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, HelloAppController],
   providers: [AppService],
 })
 export class AppModule {}
